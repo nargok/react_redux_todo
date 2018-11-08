e35b008b4cc8c14aeaf6f2dbf59adeda3f87ffb3
nargok <nargok@gmail.com>
2018-11-08 11:04:40 +0900

    complete todo functionality

---
+++ src/containers/TodoApp.js
@@ -0,0 +1,29 @@
+import { inputTask, addTask, resetTask, clickItem } from '../actions/task';
+import { connect } from 'react-redux';
+import TodoApp from '../componentes/TodoApp';
+
+const mapStateToProps = ({ task, tasks }) => {
+  return {
+    task,
+    tasks
+  }
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    inputTask: task => {
+      dispatch(inputTask(task))
+    },
+    addTask: () => {
+      dispatch(addTask())
+    },
+    resetTask: () => {
+      dispatch(resetTask())
+    },
+    clickItem: index => {
+      dispatch(clickItem(index))
+    },
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoApp);
--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import { createStore } from 'redux';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
+import tasksReducer from './reducers/task';
+import TodoApp from './containers/TodoApp';
+
+const store = createStore(tasksReducer);
+
+render (
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+);
