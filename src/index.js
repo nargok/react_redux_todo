5032f6b8d0f84737578f447daf3b2ce604934989
nargok <nargok@gmail.com>
2019-07-28 12:57:22 +0900

    test normalizr

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import tasksReducer from "./reducers/task";
 import Task from "./containers/task";
 import {createLogger} from "redux-logger";
+import { normalize, schema } from 'normalizr';
+
 
 const logger = createLogger();
 
@@ -14,6 +16,39 @@ const store = createStore(
   undefined,
   applyMiddleware(logger)
 );
+
+
+// Test normalizr
+const articlesData = {
+  "id": "123",
+  "author": {
+    "id": "1",
+    "name": "Paul"
+  },
+  "title": "My awesome blog post",
+  "comments": [
+    {
+      "id": "324",
+      "commenter": {
+        "id": "2",
+        "name": "Nicole"
+      }
+    }
+  ]
+};
+
+
+const user = new schema.Entity('users');
+const comment = new schema.Entity('comments', {
+  commenter: user
+});
+const article = new schema.Entity('articles', {
+  author: user,
+  comments: [comment]
+});
+
+const normalizedDate = normalize(articlesData, article);
+console.log(normalizedDate);
 
 render(
   <Provider store={store}>
